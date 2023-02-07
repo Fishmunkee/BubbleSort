2fccde183b40a0ff4257eb2815e3d6ec8e8318bf
Oliver Norgate <oliverpnorgate@gmail.com>
2023-02-07 16:58:59 +0000

    Trying to fix readme and also commented some lines in Test

--- src/test/java/com/sparta/BubbleSorterTest.java
+++ src/test/java/com/sparta/BubbleSorterTest.java
@@ -3,11 +3,11 @@ package com.sparta;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
+// import org.junit.jupiter.params.ParameterizedTest;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.*;
+// import static org.junit.jupiter.api.Assertions.*;
 
 class BubbleSorterTest {
     @Test
